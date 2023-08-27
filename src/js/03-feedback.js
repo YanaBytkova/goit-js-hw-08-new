7dd77121861876277dc5f343f3861781e61e420d
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-08-27 21:07:31 +0300

    after check mentor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,9 +4,7 @@ const ourForm = document.querySelector('.feedback-form')
 const inputEmail = document.querySelector("input");
 const inputMsg = document.querySelector("textarea");
 const button = document.querySelector("button");
-// console.log(ourForm);
-// console.log(inputEmail);
-// console.log(inputMsg);
+
 const oldData = JSON.parse(localStorage.getItem("feedback-form-state"));
     if (oldData != null) {
       console.log("old data from Storage:", oldData);
@@ -31,18 +29,21 @@ const currentData = function(event) {
 };
 
 ourForm.addEventListener("input", throttle(currentData,500));
+
 ourForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const form = event.currentTarget;
-  const saveEmail = form.elements.email;
-  const saveMessage = form.elements.message;
-  if (saveEmail === "" || saveMessage === "") {
-    return alert("Please fill in all the fields!");
+  const {
+      elements: { email, message }
+  } = event.currentTarget;
+
+  if (email.value === "" || message.value === "" ) {
+     return alert("Please fill in all the fields!");
   }
 
-  console.log(`Email: ${saveEmail.value}, Message: ${saveMessage.value}`);
-  form.reset();
+  console.log(`Email: ${email.value}, Message: ${message.value}`);
+
+  event.currentTarget.reset();
 }
 
